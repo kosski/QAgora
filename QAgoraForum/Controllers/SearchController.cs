83bc4df7bb484ea1ace53a7feb1866cb1e419062
iwonam <iwonaw87@gmail.com>
2015-04-21 18:11:26 +0200

    SearchController

---
+++ QAgoraForum/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QAgoraForum.Models;
+
+namespace QAgoraForum.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly Respository respository = new Respository();
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult FindUser()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult FindUser(string userName)
+        {
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                return View();
+            }
+            return View(respository.FindUser(userName));
+        }
+    }
+}
